1c150ea4d32457887e93c768fd4fef7fa9c1ecc6
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2020-06-01 19:48:57 -0700

    initial settings screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import ResetPassword from '../screens/ResetPassword';
+import Settings from '../screens/SettingsScreen';
 import SignIn from '../screens/SignInScreen';
 import SignUp from '../screens/SignUpScreen';
 
@@ -10,9 +11,10 @@ export default createAppContainer(
     createSwitchNavigator({
         // You could add another route here for authentication.
         // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-        SignIn: SignIn,
-        SignUp: SignUp,
-        ResetPassword: ResetPassword,
+        Settings,
+        SignIn,
+        SignUp,
+        ResetPassword,
         Main: MainTabNavigator,
     })
 );
