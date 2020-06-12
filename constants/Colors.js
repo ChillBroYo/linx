1df12e3e634bf5f1d39a1cd5c59404200e4a4137
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2020-06-11 17:48:02 -0700

    store app colors in constants file

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,10 @@
-const tintColor = '#2f95dc';
+// COLORS
+export const black = '#1B1B1B';
+export const green = '#439E73';
+export const grey = '#8D99AE';
+export const purple = '#8002FE';
+export const white = '#FFF';
 
-export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
-};
+// LINEAR GRADIENT COLORS
+export const darkGradient = ['#439E73', 'rgba(254, 241, 2, 0)'];
+export const lightGradient = ['rgba(254, 241, 2, 0)', 'rgba(254, 241, 2, 0.1)'];
