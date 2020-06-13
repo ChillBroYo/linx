8c3b87802a66f5168933919e50ed45836ae0b8f1
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2020-06-12 23:53:38 -0700

    include remaining settings screens

--- navigation/SettingsNavigator.js
+++ navigation/SettingsNavigator.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { createStackNavigator } from 'react-navigation';
-import SettingsHome from '../screens/SettingsHome';
-import SettingsName from '../screens/user/Name';
-import SettingsLocation from '../screens/user/Location';
+import SettingsHome from '../screens/Settings';
 import SettingsBirthday from '../screens/user/Birthday';
+import SettingsEmail from '../screen/user/Email';
 import SettingsGender from '../screens/user/Gender';
 import SettingsInterests from '../screens/user/Interests';
+import SettingsLocation from '../screens/user/Location';
+import SettingsName from '../screens/user/Name';
+import SettingsPassword from '../screen/user/Password';
+import SettingsProfile from '../screen/user/Profile';
 import TabBarIcon from '../components/TabBarIcon';
 
 const SettingsStack = createStackNavigator(
     {
         SettingsBirthday,
-        SettingsHome,
+        SettingsEmail,
         SettingsGender,
+        SettingsHome,
         SettingsInterests,
         SettingsLocation,
         SettingsName,
+        SettingsPassword,
+        SettingsProfile,
     },
     {
         headerMode: 'none',
