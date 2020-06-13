0880d39b15348fd3d754ebca48738228e9654d9a
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2020-06-13 00:41:37 -0700

    fix settings screens imports

--- navigation/SettingsNavigator.js
+++ navigation/SettingsNavigator.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import SettingsHome from '../screens/Settings';
 import SettingsBirthday from '../screens/user/Birthday';
-import SettingsEmail from '../screen/user/Email';
+import SettingsEmail from '../screens/user/Email';
 import SettingsGender from '../screens/user/Gender';
 import SettingsInterests from '../screens/user/Interests';
 import SettingsLocation from '../screens/user/Location';
 import SettingsName from '../screens/user/Name';
-import SettingsPassword from '../screen/user/Password';
-import SettingsProfile from '../screen/user/Profile';
+import SettingsPassword from '../screens/user/Password';
+import SettingsProfile from '../screens/user/Profile';
 import TabBarIcon from '../components/TabBarIcon';
 
 const SettingsStack = createStackNavigator(
