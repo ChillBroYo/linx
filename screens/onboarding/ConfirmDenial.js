269a62b139618c66be9b8728601895e183152e8b
Marco Mendez <marcomendez3800@gmail.com>
2020-06-15 20:21:46 -0700

    Main Cards Screen Added

--- screens/onboarding/ConfirmDenial.js
+++ screens/onboarding/ConfirmDenial.js
@@ -25,35 +25,34 @@ export default class ConfirmDenialScreen extends React.Component {
   }
 
   render() {
-  	return(
-  		<View style={globalStyles.outerContainer}>
-      	<LinearGradient colors={['#439E73', 'rgba(254, 241, 2, 0)']} style={{height: '100%'}}>
-      		<View style={globalStyles.innerContainer}>
-       			<View style={globalStyles.titleContainer}>
-       				<Text style={globalStyles.whiteTitle}>Let's start!</Text>
-       			</View>
-       			<View style={globalStyles.paginationContainer}>
-	     				<Image source={require('../../assets/icons/pagination_three.png')} style={globalStyles.paginationIcon} />
-	     			</View>
-       			<View style={globalStyles.contentContainer}>
-       				<Text style={globalStyles.content}>Ok. You can always verify to connect with other verified people if you change your mind.</Text>
-       			</View>
-       			<View style={globalStyles.verifyContainer}>
-       				<Text style={globalStyles.verify} onPress={() => this.props.navigation.navigate('TakeProfile')}>Verify with photo</Text>
-       			</View>
-       			<View style={globalStyles.emojiContainer}>
-       				<View style={globalStyles.emojiSymbol}>
-       					<Emoji name="tada" style={globalStyles.emojiStyle} onPress={() => this.shootCannon()} />
-       				</View>
-  					</View>
-  					<View style={styles.confettiContainer}>
+    return(
+      <View style={globalStyles.outerContainer}>
+        <LinearGradient colors={['#439E73', 'rgba(254, 241, 2, 0)']} style={{height: '100%'}}>
+          <View style={globalStyles.innerContainer}>
+            <View style={globalStyles.titleContainer}>
+              <Text style={globalStyles.whiteTitle}>Let's start!</Text>
+            </View>
+            <View style={globalStyles.paginationContainer}>
+            </View>
+            <View style={globalStyles.contentContainer}>
+              <Text style={globalStyles.content}>Ok. You can always verify to connect with other verified people if you change your mind.</Text>
+            </View>
+            <View style={globalStyles.verifyContainer}>
+              <Text style={globalStyles.verify} onPress={() => this.props.navigation.navigate('TakeProfile')}>Verify with photo</Text>
+            </View>
+            <View style={globalStyles.emojiContainer}>
+              <View style={globalStyles.emojiSymbol}>
+                <Emoji name="tada" style={globalStyles.emojiStyle} onPress={() => this.shootCannon()} />
+              </View>
+            </View>
+            <View style={styles.confettiContainer}>
               <ConfettiCannon count={100} origin={{ x: 175, y: 125 }} explosionSpeed={500} fallSpeed={2500} fadeOut={true}
-              autoStart={false} ref={this.cannon} />
-  					</View>
-  				</View>
-  			</LinearGradient>
-  		</View>
-  	);
+              autoStart={false} ref={this.cannon} onAnimationEnd={() => this.props.navigation.navigate('MainCards')} />
+            </View>
+          </View>
+        </LinearGradient>
+      </View>
+    );
   }
 }
 
