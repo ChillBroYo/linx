38de858c269ec833cc51488e8c3d9e6565f01ec4
Marco Mendez <marcomendez3800@gmail.com>
2020-06-26 20:40:09 -0700

    Fixed file changes that were not needed

--- navigation/CardsNavigator.js
+++ navigation/CardsNavigator.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 import WelcomeScreen from '../screens/onboarding/Welcome';
 import ProfileScreen from '../screens/onboarding/Profile';
