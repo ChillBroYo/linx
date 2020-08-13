aa7e5f4c778f571da01d48cae1ff9c144bd95e72
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2020-08-13 16:53:16 -0700

    change city from input to picker

--- screens/user/Location.js
+++ screens/user/Location.js
@@ -2,9 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import {
     Alert,
     Keyboard,
+    Modal,
+    Picker,
     SafeAreaView,
     ScrollView,
     StatusBar,
+    StyleSheet,
     Text,
     TextInput,
     TouchableWithoutFeedback,
@@ -45,6 +48,7 @@ export default function UserLocation({ navigation }) {
     const [state, setState] = useState(contextState);
     const [zip, setZip] = useState(contextZip);
     const [distance, setDistance] = useState(contextDistance);
+    const [showPicker, setShowPicker] = useState(false);
 
     useEffect(() => {
         StatusBar.setBarStyle(isSignUpScreen ? 'light-content' : 'dark-content');
@@ -119,20 +123,44 @@ export default function UserLocation({ navigation }) {
                     <ScrollView style={pageStyles.container}>
                         <PageHeader value='I live in' />
                         <Form>
-                            <TextInput
-                                placeholder='City'
-                                value={city}
-                                onChangeText={city => setCity(city)}
-                                clearButtonMode='while-editing'
-                                style={formStyles.input}
-                            />
-                            <TextInput
+                            <View style={formStyles.input}>
+                                <Text
+                                    onPress={() => setShowPicker(!showPicker)}
+                                    style={styles.cityText}
+                                >
+                                    {city || 'Select an area'}
+                                </Text>
+                            </View>
+                            <Modal animationType='slide' transparent={true} visible={showPicker}>
+                                <TouchableWithoutFeedback onPress={() => setShowPicker(false)}>
+                                    <View style={{flex: 1}} />
+                                </TouchableWithoutFeedback>
+                                <View style={styles.pickerWrapper}>
+                                    <View style={styles.pickerButtonWrapper}>
+                                        <Text
+                                            onPress={() => setShowPicker(false)}
+                                            style={styles.pickerButtonText}
+                                        >
+                                            Done
+                                        </Text>
+                                    </View>
+                                    <Picker
+                                        onValueChange={(itemValue) => setCity(itemValue)}
+                                        selectedValue={city}
+                                    >
+                                        <Picker.Item label='San Francisco Bay Area' value='San Francisco Bay Area' />
+                                        <Picker.Item label='Los Angeles Area' value='Los Angeles Area' />
+                                    </Picker>
+                                </View>
+                            </Modal>
+                            {/* LEAVING STATE FIELD IN FOR FUTURE */}
+                            {/*<TextInput
                                 placeholder='State'
                                 value={state}
                                 onChangeText={state => setState(state)}
                                 clearButtonMode='while-editing'
                                 style={formStyles.input}
-                            />
+                            />*/}
                             <TextInput
                                 placeholder='ZIP Code'
                                 value={zip}
@@ -164,3 +192,25 @@ export default function UserLocation({ navigation }) {
         </TouchableWithoutFeedback>
     );
 }
+
+const styles = StyleSheet.create({
+    cityText: {
+        fontSize: 20,
+    },
+    pickerButtonWrapper: {
+        alignItems: 'flex-end',
+        paddingRight: 10,
+        paddingTop: 10,
+    },
+    pickerButtonText: {
+        color: 'blue',
+        fontSize: 20,
+    },
+    pickerWrapper: {
+        backgroundColor: 'white',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        position: 'absolute',
+    },
+});
